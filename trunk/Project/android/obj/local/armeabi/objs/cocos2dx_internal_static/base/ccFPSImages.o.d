68b42b328b675515287ad08b3f8380174326c3f4
Ricard Perez del Campo <ricardperez@iMac-de-Ricard.home>
2014-10-30 23:22:00 +0000

    Removed the obj contents from the android project

--- trunk/Project/android/obj/local/armeabi/objs/cocos2dx_internal_static/base/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/armeabi/objs/cocos2dx_internal_static/base/ccFPSImages.o: \
-  jni/../../../Source/libs/cocos2d/cocos/./base/ccFPSImages.c \
-  jni/../../../Source/libs/cocos2d/cocos/./base/ccFPSImages.h
-
-jni/../../../Source/libs/cocos2d/cocos/./base/ccFPSImages.h:
--- trunk/Project/android/obj/local/armeabi/objs/cocostudio_static/CCArmatureDefine.o.d
+++
@@ -1,5 +0,0 @@
-./obj/local/armeabi/objs/cocostudio_static/CCArmatureDefine.o: \
-  jni/../../../Source/libs/cocos2d/cocos/editor-support/cocostudio/CCArmatureDefine.cpp \
-  jni/../../../Source/libs/cocos2d/cocos/editor-support/cocostudio/../cocostudio/CCArmatureDefine.h
-
-jni/../../../Source/libs/cocos2d/cocos/editor-support/cocostudio/../cocostudio/CCArmatureDefine.h:
