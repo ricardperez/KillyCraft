e0e3cb51bf6d4054ce301590c539d4cf05fee960
Ricard Perez del Campo <ricardperez@iMac-de-Ricard.home>
2014-10-30 21:55:53 +0000

    The cocos2d external were in the gitignore. Solved.

---
+++ trunk/Project/android/jni/Android.mk
@@ -0,0 +1,40 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
+
+LOCAL_MODULE := cocos2dcpp_shared
+
+LOCAL_MODULE_FILENAME := libcocos2dcpp
+
+LOCAL_SRC_FILES := hellocpp/main.cpp \
+                   ../../../Source/Game/AppDelegate.cpp \
+                   ../../../Source/Game/Command/CommandDirector.cpp \
+                   ../../../Source/Game/Gameplay/Component/Component.cpp \
+                   ../../../Source/Game/Gameplay/Component/GamepadComponent.cpp \
+                   ../../../Source/Game/Gameplay/Component/PositionComponent.cpp \
+                   ../../../Source/Game/Gameplay/Component/ViewComponent.cpp \
+                   ../../../Source/Game/Gameplay/Component/WeaponComponent.cpp \
+                   ../../../Source/Game/Gameplay/Gamepad.cpp \
+                   ../../../Source/Game/Gameplay/Map.cpp \
+                   ../../../Source/Game/Gameplay/MapObject.cpp \
+                   ../../../Source/Game/Gameplay/ObjectsFastFactory.cpp \
+                   ../../../Source/Game/Gameplay/Player.cpp \
+                   ../../../Source/Game/Gameplay/View/MapView.cpp \
+                   ../../../Source/Game/Gameplay/View/StarsView.cpp \
+                   ../../../Source/Game/Screens/GameScreen.cpp \
+                   ../../../Source/Game/Screens/MenuScreen.cpp \
+                   ../../../Source/MelonGames/Services/FilesService.cpp \
+                   ../../../Source/MelonGames/Services/Service.cpp \
+                   ../../../Source/MelonGames/Services/ServicesManager.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Source
+
+LOCAL_STATIC_LIBRARIES := cocos2dx_static
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,.)
