ee10821411a349dcf1f206df26c4d9c65d40d484
Ricard Perez <ricardperez@gmail.com>
2015-08-03 09:51:25 +0100

    Android makefile updated.

--- trunk/Project/android/jni/Android.mk
+++ trunk/Project/android/jni/Android.mk
@@ -46,11 +46,11 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
                    ../../../Source/Game/Gameplay/MapObjectsFactory.cpp \
                    ../../../Source/Game/Gameplay/MapObjectInspector.cpp \
                    ../../../Source/Game/Gameplay/MapTransitionController.cpp \
-                   ../../../Source/Game/Gameplay/VFXController.cpp \
+                   ../../../Source/Game/Gameplay/MapTime.cpp \
                    ../../../Source/Game/Gameplay/Player.cpp \
+                   ../../../Source/Game/Gameplay/VFXController.cpp \
                    ../../../Source/Game/Gameplay/PowerUp/PowerUpAction.cpp \
                    ../../../Source/Game/Gameplay/PowerUp/PowerUpFactory.cpp \
-                   ../../../Source/Game/Gameplay/SpawnManagers.cpp \
                    ../../../Source/Game/Gameplay/SpawnObjectsManager.cpp \
                    ../../../Source/Game/Gameplay/View/GameHUDView.cpp \
                    ../../../Source/Game/Gameplay/View/MapGridView.cpp \
